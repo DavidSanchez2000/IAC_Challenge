8e14fc071956d51dd4aefdb98ed81b9cad6f0626
David Sanchez <davidalejandrosanchezarias058@gmail.com>
2022-03-24 17:32:21 -0500

    fixes 7

--- script.sh
+++ script.sh
@@ -3,15 +3,5 @@
 sudo yum -y install httpd
 sudo systemctl enable httpd
 sudo systemctl start httpd.service 
-sudo echo "
-<html>
-   <head>
-      <title>IAC Challenge </title>
-   </head>
-   <body style="background-color:grey;">
-      <h1>Products</h1>
-      <p>Hola soy David Sanchez Arias y esto es mi IaC.</p>
-   </body>
-</html>"
->> /var/www/html/index.html
+sudo echo "<html><body>Hola soy David Sánchez Arias y esto es mi IaC</body></html>" >> /var/www/html/index.html
 sudo systemctl restart httpd
